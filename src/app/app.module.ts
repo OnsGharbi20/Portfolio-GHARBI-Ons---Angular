a533b7adff7670953a1e0602038d5afa09b613e5
OnsGharbi20 <125986715+OnsGharbi20@users.noreply.github.com>
2024-06-22 16:41:15 +0200

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { StatsComponent } from './components/stats/stats.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ScrollTopComponent } from './components/scroll-top/scroll-top.component';
+import { ServicesSectionComponent } from './components/services-section/services-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HeroComponent,
+    AboutComponent,
+    StatsComponent,
+    SkillsComponent,
+    ResumeComponent,
+    PortfolioComponent,
+    TestimonialsComponent,
+    ContactComponent,
+    ScrollTopComponent,
+    ServicesSectionComponent
   ],
   imports: [
     BrowserModule,
